855e09297509f8c6d84bd92a4ccffcefb4700119
westgrosh <groshev.vova@bk.ru>
2024-01-15 19:35:30 +0300

    update 15.01.2024

--- js/main.js
+++ js/main.js
@@ -8,20 +8,20 @@ function compute_date() {
     var today = new Date();
     var start_time = moment("2022-09-01");
     var end_time = moment(today);
-    var diff_time = end_time.diff(start_time, 'month');
+    var diff_time = end_time.diff(start_time, 'year');
 
-    $('#my_exp').html('Опыт: ' + diff_time + ' мес.');
+    $('#my_exp').html('Опыт: ' + diff_time + ' г.');
 }
 
-function checkMediaQuery(){
-    if ( window.innerWidth < 480 ) {
+function checkMediaQuery() {
+    if (window.innerWidth < 480) {
         $('.projects_cards_container').hide();
-        
+
         $('.section_name').hide();
         $('.part_header').show();
     } else {
         $('.projects_cards_container').show();
-        
+
         $('.section_name').show();
         $('.part_header').hide();
     }
@@ -29,6 +29,6 @@ function checkMediaQuery(){
 
 window.addEventListener('resize', checkMediaQuery);
 
-$('.part_header').click( function(){
-    $(this).next('.projects_cards_container').toggle();  
+$('.part_header').click(function () {
+    $(this).next('.projects_cards_container').toggle();
 })
